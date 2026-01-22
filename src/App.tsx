611525bcf4c98083148626923452241865b463b9
Subas_Panta <pantasubas258@gmail.com>
2026-01-22 13:57:36 +0545

    refactor: remove unused image files and comment out project routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SocialPlatformCaseStudy from "./pages/SocialPlatformCaseStudy";
+// import SocialPlatformCaseStudy from "./pages/SocialPlatformCaseStudy";
 // import AIChatCaseStudy from "./pages/AIChatCaseStudy";
 
 function App() {
@@ -8,10 +8,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
+        {/* <Route
           path="/projects/social-platform"
           element={<SocialPlatformCaseStudy />}
-        />
+        /> */}
         {/* <Route
           path="/projects/ai-chat"
           element={<AIChatCaseStudy />}
